d722482f52743ef8b80f2d152c844e49d472bd58
ilutsav <ilutsav@gmail.com>
2023-09-14 09:38:57 +0545

    conditional statements

--- firstprogram.java
+++ firstprogram.java
@@ -5,18 +5,30 @@ public class firstprogram {
        System.out.println("Enter number to test");
        Scanner sc = new Scanner(System.in);
        int n = sc.nextInt();
-        if(n==0){
-        System.out.println("number is zero");
-       }
-       else if(n/2==1){
-        System.out.println("number is even");
-       }
-       else{
-        System.out.println("number is odd");
-       }
 
-       int s = 3%2;
-       System.out.println(s);
-       
+
+       // IF ELSE //
+    //     if(n==0){
+    //     System.out.println("number is zero");
+    //    }
+    //    else if(n/2==1){
+    //     System.out.println("number is even");
+    //    }
+    //    else{
+    //     System.out.println("number is odd");
+    //    }
+
+    //    int s = 3%2;
+    //    System.out.println(s);
+
+    // SWITCH BREAK //
+    // switch(n){
+    //     case 1: System.out.println("you have entered 1");
+    //     break;
+    //     case 2: System.out.println("you have entered 2");
+    //     break;
+    //     case 3: System.out.println("invalid number");
+    //     break;
+    // }
     }
 }
