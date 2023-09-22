6e42af6e1b923dbe75034cb484a674002eb76c6a
ilutsav <ilutsav@gmail.com>
2023-09-22 16:30:16 +0545

    making multiplayer TikTakToe game

---
+++ OOPs/tictactoe/TicTacToe.java
@@ -0,0 +1,33 @@
+package OOPs.tictactoe;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class TicTacToe implements ActionListener{
+
+    Random random = new Random();
+    JFrame frame = new JFrame();
+    JPanel title_panel = new JPanel();
+    JPanel button_panel = new JPanel();
+    JLabel textField = new JLabel();
+    JButton[] buttons = new JButton[9];
+    boolean player1_turn;
+
+
+
+    TicTacToe(){}
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    }
+
+    public void firstTurn(){}
+    
+    public void check(){}
+
+    public void xWins(int a,int b,int c){}
+
+    public void oWins(int a,int b,int c){}
+}
+
